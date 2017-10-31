19bc92af0f13bcfa3ecb6555d1db2efde2fe0b28
asnani04 <asnani04@gmail.com>
2017-10-31 11:45:59 +0000

    credentials being taken

--- client_attempt.py
+++ client_attempt.py
@@ -2,7 +2,20 @@ import socket
 
 s = socket.socket()
 
+def client_auth(s):
+    msg = s.recv(1024)
+    print(msg)
+    username = raw_input()
+    s.send(username)
+    msg = s.recv(1024)
+    print(msg)
+    passwd = raw_input()
+    s.send(passwd)
+    return 0
+
 port = 10101
 s.connect(('127.0.0.1', port))
 print(s.recv(1024))
+s.send("Thanks.")
+auth = client_auth(s)
 s.close()
--- socket_attempt.py
+++ socket_attempt.py
@@ -7,10 +7,21 @@ s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind(('', port))
 s.listen(5)
 
+def authenticate(c, addr):
+    c.send("Please enter your username.")
+    username = c.recv(1024)
+    print(username)
+    c.send("Please enter your password.")
+    passwd = c.recv(1024)
+    print(passwd)
+    return 0
+
 while True:
     c, addr = s.accept()
     print("accepted connection from " + str(addr))
     c.send("You have been connected to the server.")
+    print(c.recv(1024))
+    auth = authenticate(c, addr);
     c.close()
 
     
