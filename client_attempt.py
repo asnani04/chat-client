10f386511f7971e2acf194f2957e661a36d9595e
asnani04 <asnani04@gmail.com>
2017-10-31 16:54:27 +0000

    one thread for each client

--- client_attempt.py
+++ client_attempt.py
@@ -4,6 +4,10 @@ s = socket.socket()
 
 def client_auth(s):
     msg = s.recv(1024)
+    if msg == "Authenticated":
+        return 1
+    elif msg == "Authentication failed":
+        return 0
     print(msg)
     username = raw_input()
     s.send(username)
@@ -11,11 +15,20 @@ def client_auth(s):
     print(msg)
     passwd = raw_input()
     s.send(passwd)
-    return 0
+    return client_auth(s)
 
 port = 10101
 s.connect(('127.0.0.1', port))
 print(s.recv(1024))
 s.send("Thanks.")
 auth = client_auth(s)
+if auth:
+    while True:
+        msg = raw_input()
+        print(msg)
+        if len(msg) > 2:
+            s.send(msg)
+        else:
+            break
+            
 s.close()
--- socket_attempt.py
+++ socket_attempt.py
@@ -1,4 +1,5 @@
 import socket
+import thread
 
 class Server(object):
     """
@@ -16,17 +17,25 @@ class Server(object):
         self.auth_file = auth_file
 
     def auth_check(self, username, passwd):
+        """
+        Check if the username, password corresponds to a valid user
+        returns: 1 if such a user is found, 0 if not
+        """
         fid = open(self.auth_file, "r")
         for line in fid.readlines():
             un, pw = line.strip().split(" ")
-            print(un, pw)
+            # print(un, pw)
             if un == username and pw == passwd:
                 return 1
         return 0
     
     def server_auth(self, c, addr, attempts=3):
+        """
+        Authentication handled by the server. attempts indicates the number of 
+        attempts left for this particular client.
+        """
         if attempts == 0:
-            return 0
+            return [0, '']
         c.send("Please enter your username.")
         username = c.recv(1024)
         print(username)
@@ -35,10 +44,23 @@ class Server(object):
         print(passwd)
         valid = self.auth_check(username, passwd)
         if valid == 1:
-            return 1
+            return [1, username]
         else:
             self.server_auth(c, addr, attempts-1)
 
+    def client_thread(self, client, addr, username):
+        """
+        Separate thread for each client connected to the chat room
+        """
+        while True:
+            try:
+                msg = client.recv(1024)
+                if msg:
+                    print("client sent: ", msg)
+            except:
+                continue
+        return 0
+        
     def serve(self):
         while True:
             c, addr = self.s.accept()
@@ -46,12 +68,16 @@ class Server(object):
             c.send("You have been connected to the server.")
             print(c.recv(1024))
             auth = self.server_auth(c, addr);
-            if auth == 1:
+            if auth[0] == 1:
                 print("Client successfully authenticated")
+                c.send("Authenticated")
+                thread.start_new_thread(self.client_thread, (c, addr, auth[1]))
             else:
                 # do something to stop the ip from trying to log in for some time
-                pass
-            c.close()
+                c.send("Authentication failed")
+                c.close()
+        c.close()
+        self.s.close()
 
 
 port = 10101
